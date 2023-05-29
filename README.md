f1765563f69fd8e36783af3cb007556c2adc841a
markthree <1801982702@qq.com>
2023-05-29 17:31:17 +0800

    feat: add, commit and push

--- README.md
+++ README.md
@@ -1,7 +1,26 @@
-# ng
+# g
 
 simplify git operations
 
+## Usage
+
+### install
+
+```shell
+deno install -A --unstable -rfn g https://deno.land/x/m3g/mod.ts
+```
+
+### cli
+
+```shell
+g # git add .
+
+g c "foo" # git commit -m "foo"
+
+g p # git push
+
+g -h # help information
+```
 
 ## License
 
--- mod.ts
+++ mod.ts
@@ -1,3 +1,33 @@
+import { execa } from "https://deno.land/x/ndeno@v1.2.0/src/process.ts";
+import { Command } from "https://deno.land/x/cliffy@v0.25.7/command/mod.ts";
+
+import { version } from "./src/version.ts";
+
 if (import.meta.main) {
-    console.log("init")
+  const commander = new Command()
+    .name("g")
+    .version(version)
+    .description(`simplify git operations`)
+    .action(() => {
+      execa(["git", "add", "."]);
+    });
+
+  const commit = new Command().alias("commit").description(
+    "commit with message",
+  )
+    .arguments("<message:string>")
+    .action((_, message) => {
+      execa(["git", "commit", "-m", `${message}`]);
+    });
+
+  const push = new Command().alias("push").description("push remote").action(
+    () => {
+      execa(["git", "push"]);
+    },
+  );
+
+  await commander
+    .command("p", push)
+    .command("c", commit)
+    .parse(Deno.args);
 }
