65868a4d4c5a82ffb8715eabd861360e942609bf
markthree <1801982702@qq.com>
2023-06-02 12:27:25 +0800

    feat: add clone

--- mod.ts
+++ mod.ts
@@ -12,7 +12,7 @@ if (import.meta.main) {
       execa(["git", "add", "."]);
     });
 
-  const commit = new Command().alias('m').alias("commit").description(
+  const commit = new Command().alias("m").alias("commit").description(
     "commit with message",
   )
     .arguments("<message:string>")
@@ -29,15 +29,21 @@ if (import.meta.main) {
   const release = new Command().alias("release").description(
     "release new tag",
   ).arguments("<tag:string>").action(async (_, tag) => {
-    await execa(['git', 'add', '.'])
-    await execa(['git', 'commit', '-m', "chore: update"])
-    await execa(['git', 'tag', tag])
-    await execa(['git', 'push', '--tags'])
+    await execa(["git", "add", "."]);
+    await execa(["git", "commit", "-m", "chore: update"]);
+    await execa(["git", "tag", tag]);
+    await execa(["git", "push", "--tags"]);
   });
+
+  const clone = new Command().alias("clone").description("clone remote repo")
+    .arguments("<repo:string>").action(async (_, repo) => {
+      await execa(["git", "clone", repo]);
+    });
 
   await commander
     .command("p", push)
     .command("r", release)
+    .command("l", clone)
     .command("c", commit)
     .parse(Deno.args);
 }
